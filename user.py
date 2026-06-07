e8f702c370e320b50419f48ae6424a77c0de5821
abubakr9495 <abuubakr725@gmail.com>
2026-06-08 01:08:10 +0500

    Update user.py

--- user.py
+++ user.py
@@ -445,18 +445,18 @@ async def cancel_booking_dt(msg: Message, state: FSMContext):
 async def booking_datetime(msg: Message, state: FSMContext, bot: Bot):
     data = await state.get_data()
     
-    if msg.from_user.id in user_locks:
+if msg.from_user.id in user_locks:
     await msg.answer("⏳ Kuting, so'rov qayta ishlanmoqda...")
     return
 
 user_locks.add(msg.from_user.id)
 
-    busy = await db.is_time_busy(
+busy = await db.is_time_busy(
         data["room"],
         msg.text.strip()
     )
 
-    if busy:
+ if busy:
         await msg.answer(
             "❌ Bu vaqt band!\n\nBoshqa vaqt tanlang."
         )
