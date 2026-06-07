b85924c19fac9ed23dcd284eaca61752e9aae27b
abubakr9495 <abuubakr725@gmail.com>
2026-06-08 01:15:46 +0500

    Update user.py

--- user.py
+++ user.py
@@ -456,13 +456,13 @@ busy = await db.is_time_busy(
         msg.text.strip()
     )
 
- if busy:
-        await msg.answer(
+if busy:
+       await msg.answer(
             "❌ Bu vaqt band!\n\nBoshqa vaqt tanlang."
         )
         return
 
-    try:
+try:
         booking_id = await db.add_booking(
             msg.from_user.id,
             data["full_name"],
