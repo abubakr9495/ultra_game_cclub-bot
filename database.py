c22f72bb1fa66872a95122f74b90669890b12655
abubakr9495 <abuubakr725@gmail.com>
2026-06-07 20:17:51 +0500

    Update database.py

--- database.py
+++ database.py
@@ -76,17 +76,6 @@ async def get_user(telegram_id: int):
 
             return user
 
-async def create_user(telegram_id: int, full_name: str, phone: str, referrer_id=None):
-    async with aiosqlite.connect(DB_PATH) as db:
-        await db.execute(
-            "INSERT OR IGNORE INTO users (telegram_id, full_name, phone, referrer_id) VALUES (?,?,?,?)",
-            (telegram_id, full_name, phone,referrer_id)
-        )
-        await db.execute(
-            "INSERT OR IGNORE INTO bonuses (user_id, amount) VALUES (?,0)",
-            (telegram_id,)
-        )
-
         async def create_user(telegram_id: int, full_name: str, phone: str, referrer_id=None):
             async with aiosqlite.connect(DB_PATH) as db:
 
